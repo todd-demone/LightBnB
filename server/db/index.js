a3dfdc7d37c3a7ef519dc7d1aca525e34bd28c2c
Todd Demone <todd.demone.dev@gmail.com>
2022-01-27 14:28:43 +0000

    add logging functionality for db queries

--- server/db/index.js
+++ server/db/index.js
@@ -10,8 +10,15 @@ const pool = new Pool({
 
 module.exports = {
   query: (text, params, fulfillmentHandler, rejectionHandler) => {
+    const start = Date.now();
     return pool
     .query(text, params)
+    // log the query, how long it took and number of rows returned
+    .then(res => {
+      const duration = Date.now() - start;
+      console.log('executed query', { text, duration, rows: res.rowCount });
+      return res;
+    })
     .then(fulfillmentHandler)
     .catch(rejectionHandler);
   },
